c5c8162df100368d526b4b365e6d69fbda529c63
marcosfaria19 <marcos.faria19@hotmail.com>
2024-07-23 08:23:05 -0300

    Configuracao tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,17 +2,6 @@ const { default: daisyui } = require("daisyui");
 
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      transitionProperty: {
-        background: "background-color",
-      },
-      transitionDuration: {
-        200: "200ms",
-      },
-    },
-  },
   plugins: [require("daisyui")],
-
   daisyui: { themes: ["light"] },
 };
