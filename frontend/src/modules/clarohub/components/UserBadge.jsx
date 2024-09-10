289398df436214ec32202ea99711d46ea60f1825
marcosfaria19 <marcos.faria19@hotmail.com>
2024-09-10 15:36:57 -0300

    feat: componente userBadge e badge aprimorados

--- frontend/src/modules/clarohub/components/UserBadge.jsx
+++ frontend/src/modules/clarohub/components/UserBadge.jsx
@@ -3,32 +3,30 @@ import { Badge } from "modules/shared/components/ui/badge";
 import { Shield, BarChart, User, UserCheck } from "lucide-react";
 
 const UserBadge = ({ permission }) => {
-  let badgeVariant;
   let IconComponent;
 
   switch (permission) {
     case "admin":
-      badgeVariant = "destructive";
       IconComponent = Shield;
       break;
     case "manager":
-      badgeVariant = "default";
       IconComponent = BarChart;
       break;
     case "basic":
-      badgeVariant = "primary";
-      IconComponent = User;
+      IconComponent = UserCheck;
       break;
     case "guest":
-      badgeVariant = "secondary";
-      IconComponent = UserCheck;
+      IconComponent = User;
       break;
     default:
       return null;
   }
 
   return (
-    <Badge variant={badgeVariant} className="items-center justify-center">
+    <Badge
+      variant={permission}
+      className="flex w-24 items-center justify-center"
+    >
       <IconComponent className="mr-1 h-4 w-4" />
       <span>{permission}</span>
     </Badge>
--- frontend/src/modules/shared/components/ui/badge.jsx
+++ frontend/src/modules/shared/components/ui/badge.jsx
@@ -4,21 +4,21 @@ import { cva } from "class-variance-authority";
 import { cn } from "modules/shared/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 select-none",
   {
     variants: {
       variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        secondary:
+        guest:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
+        basic:
+          "border-transparent bg-accent text-accent-foreground hover:bg-accent/80",
+        manager:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
+        admin: "text-foreground",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "guest",
     },
   },
 );
