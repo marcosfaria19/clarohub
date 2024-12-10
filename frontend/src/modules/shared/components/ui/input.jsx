c93c43fef2e0cf28ac0a6a5a9930c6786fb56944
marcosfaria19 <marcos.faria19@hotmail.com>
2024-12-10 09:29:04 -0300

    fix: [Hub] Correção do bug dos labels floating não funcionarem quando o navegator tiver preenchendo os inputs com AutoFill

--- frontend/src/modules/shared/components/ui/input.jsx
+++ frontend/src/modules/shared/components/ui/input.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { cva } from "class-variance-authority";
 import { cn } from "modules/shared/lib/utils";
+import { useState } from "react";
 
 const inputVariants = cva(
   "border rounded pl-3 outline-none transition duration-200 ease-in select-none appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [-moz-appearance:textfield]",
@@ -21,10 +22,12 @@ const inputVariants = cva(
 
 const Input = React.forwardRef(
   ({ className, type, label, floating, variant, ...props }, ref) => {
-    const [isFocused, setIsFocused] = React.useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+    const [isAutoFilled, setIsAutoFilled] = useState(false);
 
     const handleFocus = () => setIsFocused(true);
     const handleBlur = () => setIsFocused(false);
+    const handleAutoFill = () => setIsAutoFilled(true);
 
     const combinedClassNames = cn(
       inputVariants({ variant }),
@@ -36,7 +39,7 @@ const Input = React.forwardRef(
       "absolute left-3.5 top-3 transition-all duration-150 ease-in-out pointer-events-none text-black",
       {
         "text-xs transform -translate-y-6 bg-white rounded px-1 text-black":
-          isFocused || props.value,
+          isFocused || props.value || isAutoFilled,
       },
     );
 
@@ -48,11 +51,29 @@ const Input = React.forwardRef(
           placeholder={label}
           onFocus={handleFocus}
           onBlur={handleBlur}
+          onAnimationStart={(e) => {
+            if (e.animationName === "onAutoFillStart") {
+              handleAutoFill();
+            }
+          }}
           {...props}
           ref={ref}
           translate="no"
         />
         {floating && <label className={floatingLabelClasses}>{label}</label>}
+        <style jsx>{`
+          @keyframes onAutoFillStart {
+            from {
+              /**/
+            }
+            to {
+              /**/
+            }
+          }
+          input:-webkit-autofill {
+            animation-name: onAutoFillStart;
+          }
+        `}</style>
       </div>
     );
   },
