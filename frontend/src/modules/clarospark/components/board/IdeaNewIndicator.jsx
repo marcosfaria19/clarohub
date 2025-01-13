312dd16ec3ed44c29df72d630c3c9e107829a3e2
marcosfaria19 <marcos.faria19@hotmail.com>
2025-01-13 09:53:20 -0300

    style: [Spark] Elemento "Nova" mais discreto

--- frontend/src/modules/clarospark/components/board/IdeaNewIndicator.jsx
+++ frontend/src/modules/clarospark/components/board/IdeaNewIndicator.jsx
@@ -8,11 +8,11 @@ export default function NewIndicator({ isNew }) {
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.3 }}
-      className="absolute right-2 top-2 flex items-center space-x-1 rounded-full bg-gradient-to-r from-pink-500 to-purple-500 px-3 py-1 text-xs font-medium text-white shadow-lg"
+      className="absolute right-0 top-[-10px] flex items-center space-x-1 rounded-full px-3 py-1 text-xs font-medium text-foreground"
     >
       <span className="relative flex h-2 w-2">
-        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-white opacity-75"></span>
-        <span className="relative inline-flex h-2 w-2 rounded-full bg-white"></span>
+        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-foreground opacity-75"></span>
+        <span className="relative inline-flex h-2 w-2 rounded-full bg-[#a344c7]"></span>
       </span>
       <span>Nova!</span>
     </motion.div>
