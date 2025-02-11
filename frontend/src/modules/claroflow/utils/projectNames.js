b0fbb7231ea862418a4651ebcdb4f6ddd6fc40ec
marcosfaria19 <marcos.faria19@hotmail.com>
2025-02-11 10:45:43 -0300

    feat: Renderização dinamica dos boards mapeando as demandas no utils boardRegistry

--- frontend/src/modules/claroflow/utils/projectNames.js
+++ frontend/src/modules/claroflow/utils/projectNames.js
@@ -1,21 +1,3 @@
-export const projects = [
-  {
-    title: "TAP",
-    image: "bgs/tap.jpg",
-    path: "/board/tap",
-  },
-  {
-    title: "MDU",
-    image: "bgs/mdu.jpg",
-    path: "/board/mdu",
-  },
-  {
-    title: "NAP",
-    image: "bgs/nap.jpg",
-    path: "/board/nap",
-  },
-];
-
 export const regionals = [
   "RMG",
   "RSI",
