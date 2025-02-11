912b938f2c5b7fad6c3eef182a5260f54c4dd4c2
marcosfaria19 <marcos.faria19@hotmail.com>
2025-02-11 13:31:19 -0300

    style: [Flow] Placeholder de card de informações da demanda e melhorias gerais da UI

--- frontend/src/modules/claroflow/components/workflow-boards/GenericBoard.jsx
+++ frontend/src/modules/claroflow/components/workflow-boards/GenericBoard.jsx
@@ -22,11 +22,11 @@ export default function GenericBoard({ assignmentId, projectId }) {
   const teamMembers = getUsersByProjectAndAssignment(projectId, assignmentId);
 
   return (
-    <div className="flex h-full flex-col gap-4 p-4 md:flex-row">
+    <div className="flex h-full flex-col gap-4 p-4 drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] md:flex-row">
       {/* Primeira Coluna */}
       <div className="flex w-full flex-col gap-4 md:w-[300px]">
         {/* Card Fila */}
-        <Card className="flex h-48 flex-col justify-between border-border bg-card-spark text-card-foreground">
+        <Card className="flex h-48 flex-col justify-between border-border bg-secondary text-card-foreground">
           <CardHeader className="pb-2">
             <CardTitle className="text-lg">
               Fila de GENERIC
@@ -34,20 +34,16 @@ export default function GenericBoard({ assignmentId, projectId }) {
                 ?.name || "Carregando..."}
             </CardTitle>
           </CardHeader>
-          <CardContent className="flex flex-1 flex-col justify-between">
-            <div className="flex-1 text-sm text-muted-foreground">
-              Pendentes: <span className="text-primary">24</span>
-            </div>
-            <div className="flex justify-end">
-              <Button variant="primary" size="sm">
-                Tratar →
-              </Button>
-            </div>
+          <CardContent className="flex justify-between text-3xl">
+            <span className="text-card-foreground">24</span>
+            <Button variant="primary" size="sm">
+              Tratar →
+            </Button>
           </CardContent>
         </Card>
 
         {/* Card Meu Time */}
-        <Card className="h-full border-border bg-card-spark text-card-foreground">
+        <Card className="h-full border-border bg-secondary text-card-foreground">
           <CardHeader className="pb-2">
             <CardTitle className="mb-4 text-lg">Meu Time</CardTitle>
           </CardHeader>
@@ -60,7 +56,10 @@ export default function GenericBoard({ assignmentId, projectId }) {
               <div className="flex flex-col gap-3">
                 {/* Lista de membros com avatares e nomes */}
                 {teamMembers.slice(0, 5).map((user) => (
-                  <div key={user._id} className="flex items-center gap-3">
+                  <Card
+                    key={user._id}
+                    className="flex items-center gap-3 p-2 drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)]"
+                  >
                     <Avatar className="h-8 w-8 border-2 border-background">
                       <AvatarImage src={user.avatar} alt={user.NOME} />
                       <AvatarFallback>
@@ -70,19 +69,19 @@ export default function GenericBoard({ assignmentId, projectId }) {
                       </AvatarFallback>
                     </Avatar>
                     <span className="text-sm">{formatUserName(user.NOME)}</span>
-                  </div>
+                  </Card>
                 ))}
 
                 {/* Indicador de overflow */}
                 {teamMembers.length > 5 && (
-                  <div className="flex items-center gap-3">
+                  <Card className="flex items-center gap-3 p-2 drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)]">
                     <div className="text-md flex h-8 w-8 items-center justify-center rounded-full border-2 border-background bg-[#edd0af] font-medium text-board-title">
                       +{teamMembers.length - 5}
                     </div>
                     <span className="text-sm text-muted-foreground">
                       {teamMembers.length - 5} membros adicionais
                     </span>
-                  </div>
+                  </Card>
                 )}
               </div>
             )}
@@ -91,19 +90,22 @@ export default function GenericBoard({ assignmentId, projectId }) {
       </div>
 
       {/* Segunda Coluna - Em Tratamento */}
-      <Card className="flex-1 border-border bg-card-spark text-card-foreground">
+      <Card className="flex-1 border-border bg-secondary text-card-foreground">
         <CardHeader className="pb-2">
           <CardTitle className="text-lg">Em Tratamento</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="h-[550px] rounded-lg border-2 border-dashed border-border p-4 text-center text-muted-foreground">
-            Área para listagem de itens em tratamento
+          <div className="h-[550px] space-y-4 rounded-lg border-2 border-dashed border-border p-4 text-center text-muted-foreground">
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
           </div>
         </CardContent>
       </Card>
 
       {/* Terceira Coluna - Finalizadas */}
-      <Card className="flex-1 border-border bg-card-spark text-card-foreground">
+      <Card className="flex-1 border-border bg-secondary text-card-foreground">
         <CardHeader className="pb-2">
           <CardTitle className="text-lg">Finalizadas</CardTitle>
         </CardHeader>
@@ -115,8 +117,11 @@ export default function GenericBoard({ assignmentId, projectId }) {
             />
             <SearchIcon className="absolute right-3 top-3 h-4 w-4 text-muted-foreground" />
           </div>
-          <div className="h-[500px] rounded-lg border-2 border-dashed border-border p-4 text-center text-muted-foreground">
-            Área para listagem de itens finalizados
+          <div className="h-[550px] space-y-4 rounded-lg border-2 border-dashed border-border p-4 text-center text-muted-foreground">
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
+            <Card className="min-h-28 rounded-lg bg-board"></Card>
           </div>
         </CardContent>
       </Card>
