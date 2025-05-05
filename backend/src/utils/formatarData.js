bfc90a8b522aabe7d9c8e9ed850c07c3946a4031
marcosfaria19 <marcos.faria19@hotmail.com>
2025-05-05 11:37:04 -0300

    feat: [Flow] Adiciona conversão de data para o campo DATA_INICIO da extração do SGD

--- backend/src/utils/formatarData.js
+++ backend/src/utils/formatarData.js
@@ -1,4 +1,4 @@
-// Função para formatar a data
+// Função para formatar a data numérica (como Excel faz)
 const formatarData = (dataNumerica) => {
   // Converter a data numérica para objeto de data
   const data = new Date((dataNumerica - 25569) * 86400 * 1000);
@@ -14,4 +14,11 @@ const formatarData = (dataNumerica) => {
   return `${dia}/${mes}/${ano} ${horas}:${minutos}`;
 };
 
-module.exports = formatarData;
+// Função para converter string de data no formato "dd/MM/yyyy HH:mm:ss" para Date
+function parseCustomDateFromExcel(dateNum) {
+  const data = new Date((dateNum - 25569) * 86400 * 1000); // Conversão do número para a data em JavaScript
+
+  return data;
+}
+
+module.exports = { formatarData, parseCustomDateFromExcel };
