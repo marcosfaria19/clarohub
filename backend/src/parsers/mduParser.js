683422cd8ac3cc527b581635a215cfd8540e865b
marcosfaria19 <marcos.faria19@hotmail.com>
2025-05-22 13:34:07 -0300

    refactor: [Flow] Simplifica a lógica de filtragem do Parser de MDU

--- backend/src/parsers/mduParser.js
+++ backend/src/parsers/mduParser.js
@@ -1,7 +1,6 @@
 const { parseCustomDateFromExcel } = require("../utils/formatarData");
 
 module.exports = function mduParser(rawData, cidadeMap, project, assignment) {
-  // Colunas esperadas e filtro de fila por assignment.name
   const spec = {
     Análise: {
       required: ["IDDEMANDA", "COD_OPERADORA", "ENDERECO_VISTORIA", "FILA"],
@@ -14,12 +13,12 @@ module.exports = function mduParser(rawData, cidadeMap, project, assignment) {
   };
 
   const cfg = spec[assignment.name];
-  if (!cfg)
+  if (!cfg) {
     throw new Error(
       `Sem configuração de parser para assignment ${assignment.name}`
     );
+  }
 
-  // Validar colunas
   if (rawData.length > 0) {
     const missing = cfg.required.filter((c) => !(c in rawData[0]));
     if (missing.length) {
@@ -29,54 +28,48 @@ module.exports = function mduParser(rawData, cidadeMap, project, assignment) {
     }
   }
 
-  // Processar rows válidos
-  return (
-    rawData
-      /* Filtros para itens não tratados ainda pela equipe de MDU */
-      .filter((row) => {
-        row.COD_BAIXA !== 209;
-      })
-      .filter((row) => {
-        row.TIPO_DEMANDA !== "Empresarial" &&
-          row.TIPO_DEMANDA !== "Inteligência de Mercado" &&
-          row.TIPO_DEMANDA !== "Projeto F";
-      })
+  let filtered = rawData;
 
-      /* Filtro para filas padrão da demanda e endereços não vazios */
-      .filter((row) => cfg.filaValues.includes(row.FILA?.trim()))
-      .filter((row) => row.ENDERECO_VISTORIA?.trim())
+  filtered = filtered.filter((row) => row.COD_BAIXA !== 209);
 
-      .map((row) => {
-        const codOper = (row.COD_OPERADORA || row.COD_OPERADORA_NODE)
-          .toString()
-          .trim();
-        const city = cidadeMap.get(codOper) || {};
-
-        // Transformação de data do excel em data sem hh/mm/ss, para correta ordenação de prioridades
-        const rawDate = row.DATA_INICIO
-          ? parseCustomDateFromExcel(row.DATA_INICIO)
-          : null;
-
-        const createdAt = rawDate
-          ? new Date(
-              rawDate.getFullYear(),
-              rawDate.getMonth(),
-              rawDate.getDate()
-            )
-          : null;
-
-        return {
-          IDDEMANDA: row.IDDEMANDA,
-          COD_OPERADORA: codOper,
-          ENDERECO_VISTORIA: row.ENDERECO_VISTORIA,
-          ...city,
-          project: { _id: project._id, name: project.name },
-          status: { _id: assignment._id, name: assignment.name },
-          assignedTo: null,
-          updatedAt: new Date(),
-          createdAt,
-          history: [],
-        };
-      })
+  filtered = filtered.filter(
+    (row) =>
+      row.TIPO_DEMANDA !== "Empresarial" &&
+      row.TIPO_DEMANDA !== "Inteligência de Mercado" &&
+      row.TIPO_DEMANDA !== "Projeto F"
   );
+
+  filtered = filtered.filter((row) =>
+    cfg.filaValues.includes(row.FILA?.trim())
+  );
+
+  filtered = filtered.filter((row) => row.ENDERECO_VISTORIA?.trim());
+
+  return filtered.map((row) => {
+    const codOper = (row.COD_OPERADORA || row.COD_OPERADORA_NODE)
+      .toString()
+      .trim();
+    const city = cidadeMap.get(codOper) || {};
+
+    const rawDate = row.DATA_INICIO
+      ? parseCustomDateFromExcel(row.DATA_INICIO)
+      : null;
+
+    const createdAt = rawDate
+      ? new Date(rawDate.getFullYear(), rawDate.getMonth(), rawDate.getDate())
+      : null;
+
+    return {
+      IDDEMANDA: row.IDDEMANDA,
+      COD_OPERADORA: codOper,
+      ENDERECO_VISTORIA: row.ENDERECO_VISTORIA,
+      ...city,
+      project: { _id: project._id, name: project.name },
+      status: { _id: assignment._id, name: assignment.name },
+      assignedTo: null,
+      updatedAt: new Date(),
+      createdAt,
+      history: [],
+    };
+  });
 };
