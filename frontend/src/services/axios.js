be1b5720bec3ca44e64f69c087df44f16ee4a827
marcosfaria19 <marcos.faria19@hotmail.com>
2025-06-30 10:29:01 -0300

    refactor: [Auth] Implementação do proxy reverso do netlify para o vercel, incluindo a rota /api no public/_redirects, e adicionando o Bearer no token de autenticação no frontend (axiosInstance) e backend (authMiddleware), a fim de eliminar requests 204 do servidor

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,31 +1,38 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
-import { Navigate } from "react-router-dom";
 
-const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-});
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_BACKEND_URL
+    : "/api";
+
+const axiosInstance = axios.create({ baseURL });
 
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      const decodedToken = jwtDecode(token);
-      const currentTime = Date.now() / 1000;
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
 
-      if (decodedToken.exp < currentTime) {
+        if (decodedToken.exp < currentTime) {
+          localStorage.removeItem("token");
+          window.location.href = "/login";
+          return Promise.reject(new Error("Token expirado"));
+        }
+
+        /* config.headers.Authorization = `${token}`; */
+        config.headers.Authorization = `Bearer ${token}`;
+      } catch (error) {
         localStorage.removeItem("token");
-        <Navigate to="/login" />;
-        return Promise.reject(new Error("Token expirado"));
+        window.location.href = "/login";
+        return Promise.reject(new Error("Token inválido"));
       }
-
-      config.headers.Authorization = token;
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  },
+  (error) => Promise.reject(error),
 );
 
 export default axiosInstance;
